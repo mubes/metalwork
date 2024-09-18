365caf4050a22945e540d2dcd042fda2f3d53a0c
Dave Marples <dave@marples.net>
2024-09-18 21:16:14 +0100

    Version after some clippy love

--- cobs/src/lib.rs
+++ cobs/src/lib.rs
@@ -1,4 +1,4 @@
-//! Consistent Overhead Byte Stream (COBS) Decoder
+//! Consistent Overhead Byte Stream (COBS) Encoder/Decoder
 //!
 //! Turns an arbitary stream into a sequence of packets.  COBS is a cheap and
 //! easy mechanism to 'packetise' a stream. If sync is lost it will automatically
@@ -19,9 +19,10 @@ use std::vec::Vec;
 mod test_lib;
 
 /// Current state of the decoder
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
 enum DecoderState {
     /// Waiting for a packet to start (i.e. a non-sentinel byte to arrive)
+    #[default]
     Idle,
     /// In the process of receiving a packet
     Rxing,
@@ -44,8 +45,8 @@ enum TokenResult {
     Complete,
 }
 
-// The decoder object
-#[derive(Debug, Clone, Eq, Copy, PartialEq)]
+/// The COBS encoder/decoder object
+#[derive(Default, Debug, Clone, Eq, Copy, PartialEq)]
 pub struct Cobs {
     state: DecoderState, // Current state of the decoder
     sentinel: u8,        // Sentinel value to be used (normally 0)
@@ -114,16 +115,9 @@ impl Cobs {
     /// handler state will be set to be waiting for the start of a packet.
     ///
     pub fn new() -> Cobs {
-        Cobs {
-            state: DecoderState::Idle,
+        Self {
             sentinel: DEFAULT_SENTINEL,
-            rxc: 0,
-            inbytes: 0,
-            goodbytes: 0,
-            badbytes: 0,
-            packets: 0,
-            toolong: 0,
-            maxcount: false,
+            ..Default::default()
         }
     }
 
@@ -291,7 +285,7 @@ impl Cobs {
     /// assert!(v == result);
     /// ```
     ///
-    pub fn get_byte<'a>(&mut self, c: u8, op: &'a mut Vec<u8>) -> Result<(), CobsError> {
+    pub fn get_byte(&mut self, c: u8, op: &mut Vec<u8>) -> Result<(), CobsError> {
         self.inbytes += 1;
         let (val, action) = self.process_token(c);
         match action {
@@ -345,7 +339,7 @@ impl Cobs {
 
             /* === Receiving a run */
             DecoderState::Rxing => {
-                self.rxc = self.rxc - 1;
+                self.rxc -= 1;
                 if 0 == self.rxc {
                     if self.sentinel == tok {
                         self.state = DecoderState::Idle;
@@ -360,13 +354,11 @@ impl Cobs {
                         self.maxcount = tok == 255;
                         (self.sentinel, action)
                     }
+                } else if self.sentinel == tok {
+                    self.state = DecoderState::Flushing;
+                    (tok, TokenResult::Error)
                 } else {
-                    if self.sentinel == tok {
-                        self.state = DecoderState::Flushing;
-                        (tok, TokenResult::Error)
-                    } else {
-                        (tok, TokenResult::Store)
-                    }
+                    (tok, TokenResult::Store)
                 }
             }
 
@@ -426,12 +418,12 @@ impl Cobs {
     /// let test_encoded = dec.cobs_encode_into_vec( &unencoded ).unwrap();
     /// assert!(encoded == test_encoded);
     ///
-    pub fn cobs_encode_into_vec(self, ip: &Vec<u8>) -> Result<Vec<u8>, CobsError> {
+    pub fn cobs_encode_into_vec(self, ip: &[u8]) -> Result<Vec<u8>, CobsError> {
         let mut e = Vec::<u8>::with_capacity(MAX_ENC_PACKET_LEN);
         match self.cobs_encode(ip, &mut e) {
-            Ok(_s) => return Ok(e),
-            Err(r) => return Err(r),
-        };
+            Ok(_s) => Ok(e),
+            Err(r) => Err(r),
+        }
     }
 
     /// Encode cobs packet
@@ -457,10 +449,10 @@ impl Cobs {
     ///
     pub fn cobs_encode<'a>(
         self,
-        ip: &'a Vec<u8>,
+        ip: &'a [u8],
         e: &'a mut Vec<u8>,
     ) -> Result<&'a mut Vec<u8>, CobsError> {
-        if ip.len() == 0 {
+        if ip.is_empty() {
             Err(CobsError::ZeroLength)
         } else if Self::max_possible_enc_len(ip.len()) > MAX_ENC_PACKET_LEN {
             Err(CobsError::Overlong)
@@ -468,7 +460,7 @@ impl Cobs {
             let mut d: usize = 0; // Position for size pointer to end of slice
             e.push(self.sentinel); // Make room for initial stride byte
 
-            for (_, i) in ip.iter().enumerate() {
+            for i in ip.iter() {
                 /* Deal with case of 0xff bytes with no sentinel - start a new run */
                 if e.len() - d == 0xff {
                     e[d] = (e.len() - d) as u8;
